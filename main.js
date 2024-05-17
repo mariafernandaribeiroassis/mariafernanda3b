a27bf6195739709b95d3840d00fd02acf0633de2
mariafernandaribeiroassis <144929768+mariafernandaribeiroassis@users.noreply.github.com>
2024-05-17 09:53:23 -0300

    .

--- main.js
+++ main.js
@@ -21,25 +21,6 @@ const tempoObjetivo3 = new Date("2025-01-10T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3];
 
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos]
-    } else {
-        return [0,0,0,0];
-    }
-}
-
 function atualizaCronometro(){
 
     for (let i=0; i<contadores.length;i++){
