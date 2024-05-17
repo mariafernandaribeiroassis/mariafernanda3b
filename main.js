03d328ba24cb6cf0455b9cccd4cb81921ce48519
mariafernandaribeiroassis <144929768+mariafernandaribeiroassis@users.noreply.github.com>
2024-05-17 09:42:57 -0300

    .

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const tempoObjetivo1 = new Date("2025-04-27T00:00:00");
 const tempoObjetivo2 = new Date("2024-06-18T00:00:00");
 const tempoObjetivo3 = new Date("2025-01-10T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3];
 
 
 function calculaTempo(tempoObjetivo) {
